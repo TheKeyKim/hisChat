67aaafa7a8faf74d10427df08fcd9dae191274d8
thekey.kim <thekey.kim@linecorp.com>
2022-04-18 20:25:13 +0900

    First commit

---
+++ src/main/java/com/loa/hischat/HisChatApplication.java
@@ -0,0 +1,13 @@
+package com.loa.hischat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loa/hischat/HisChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loa.hischat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
